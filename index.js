6af1bc1a362e98236b8a1c9e3647b07b44cf19e8
naseefxyz <naseef@appler.xyz>
2018-07-14 22:53:39 +0530

    first logic added

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-const express = require("express");
+// require modules
+const express = require('express');
+const { Gpio } = require('onoff');
 
-
+// express app
 const app = express();
 
+// RPi pins
+const LED1 = new Gpio(17, 'out');
+
+// endpoints
 app.get('/', (req, res) => {
   res.redirect('/home');
-})
+});
 
 app.get('/home', (req, res) => {
-  res.send("home automation center");
-})
+  res.send('home automation center');
+});
 
 app.get('/toggle/:lightid', (req, res) => {
   const lightid = req.params.lightid;
   // logic to toggle the light
 
+  const led1Status = LED1.readSync();
+  if (led1Status) {
+    LED1.writeSync(0);
+  } else {
+    LED1.writeSync(1);
+  }
+
   res.json({
     value: lightid,
-    toggled: true
+    toggled: true,
+    currentStatus: !led1Status,
   });
-})
-
-
+});
 
 app.listen('5500');
